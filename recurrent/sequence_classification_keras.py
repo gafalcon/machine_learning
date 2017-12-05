9020018d2d477b36b36b8a33dd1351529c883c18
Gabriel Falcones <gafalcon@espol.edu.ec>
2017-12-05 11:58:41 -0500

    sequence classification with LSTM and Conv

--- recurrent/sequence_classification_keras.py
+++ recurrent/sequence_classification_keras.py
@@ -4,6 +4,7 @@ import numpy
 from keras.datasets import imdb
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout
+from keras.layers.convolutional import Conv1D, MaxPooling1D
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 
@@ -25,7 +26,10 @@ embedding_vector_length = 32
 model = Sequential()
 model.add(Embedding(top_words, embedding_vector_length, input_length=max_review_length))
 # model.add(Dropout(0.2))
-model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
+model.add(MaxPooling1D(pool_size=2))
+model.add(LSTM(100))
+# model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
 # model.add(Dropout(0.2))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
